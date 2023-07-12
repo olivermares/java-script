5895460d8e03a72360f7c8bd63ccda2dfd8eb050
olivermares <olivermares@hotmail.com>
2023-07-12 17:00:25 +0200

    segundo dia de java script

---
+++ javascript-exercises-main/S2/extra/ex4-a.js
@@ -0,0 +1,29 @@
+
+/*
+Crea una función llamada `findArrayIndex` que reciba como parametros un array de textos y un texto y devuelve la 
+posición del array cuando el valor del array sea igual al valor del texto que enviaste como parametro.
+
+Haz varios ejemplos y compruebalos.
+
+Sugerencia de función:
+
+```js
+function findArrayIndex(array, text) {}
+```
+
+Ej array:
+````js
+['Caracol', 'Mosquito', 'Salamandra', 'Ajolote']
+*/
+
+
+
+function findArrayIndex(array, text) {
+
+}
+let ejemplos =[['Caracol', 'Mosquito', 'Salamandra', 'Ajolote'], ["Saab", "Volvo", "BMW"], ['Japon', 'Venecia', 'Murcia', 'Santander', 'Filipinas', 'Madagascar']]
+let texts = ['mosquito' , 'bmw', 'no']
+
+for (const ejemplo of ejemplos) {
+    console.log(findArrayIndex(ejemplo,ejemplo[0]));
+}
